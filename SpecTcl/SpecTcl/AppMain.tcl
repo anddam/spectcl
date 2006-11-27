f96e9b4cb2c3c232b2aecf53e45546e0c6c956de
darmou <darmou@a36c15cb-1223-0410-88a0-9bd82a4b9a45>
2006-11-27 12:14:38 +0000

    used for creating Os X apps darmou

---
+++ SpecTcl/SpecTcl/AppMain.tcl
@@ -0,0 +1,5 @@
+if {[string first "-psn" [lindex $argv 0]] == 0} { set argv [lrange $argv 1 end]}
+
+console show
+
+if [catch {source [file join [file dirname [info script]] main.tk]}] { puts $errorInfo}
